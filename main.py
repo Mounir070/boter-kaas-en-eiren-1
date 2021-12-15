4df1df72d0dfcf44e90f404685976420907c14fc
MounirKalai <57255735+Mounir070@users.noreply.github.com>
2021-12-15 10:54:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from _ml import MLAgent, train, save, load, RandomAgent, train_and_plot
+from _core import is_winner, opponent, start
+
+import random
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
+    
+    
+random.seed(1)
+ 
+my_agent = MyAgent()
+random_agent = RandomAgent()
+ 
+train_and_plot(
+    agent=my_agent,
+    validation_agent=random_agent,
+    iterations=50,
+    trainings=100,
+    validations=1000)
